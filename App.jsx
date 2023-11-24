0511e2af1057f136867c013530b6f358cecfe85c
Arul <official@arul.me>
2023-11-24 20:51:39 +0800

    nice

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  };
+  const sub = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <html>
+      <h1>hello</h1>
+      <button onClick={add}>Increment</button>
+      <button onClick={sub}>Decrement</button>
+    </html>
+  );
+}
